690288691d6fd4699d82ceead39fdf59afff30d5
thomas_lorthioir <tottilorthioir@gmail.com>
2022-12-04 18:18:13 +0100

    settings + contact fragment

---
+++ app/src/main/java/com/example/slowchien/ui/contact/ContactFragment.java
@@ -0,0 +1,36 @@
+package com.example.slowchien.ui.contact;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.slowchien.databinding.FragmentContactsBinding;
+
+public class ContactFragment extends Fragment {
+    private FragmentContactsBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        ContactViewModel contactViewModel =
+                new ViewModelProvider(this).get(ContactViewModel.class);
+
+        binding = FragmentContactsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textContacts;
+        contactViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
