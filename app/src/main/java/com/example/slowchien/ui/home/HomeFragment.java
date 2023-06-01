5d54b2fa769124a4e473d908ed954eb6f3acce37
loic mouquet <loic.mouquet59@gmail.com>
2023-06-01 10:49:33 +0200

    add new message button

--- app/src/main/java/com/example/slowchien/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/slowchien/ui/home/HomeFragment.java
@@ -4,15 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.viewpager.widget.ViewPager;
 
 import com.example.slowchien.R;
 import com.example.slowchien.databinding.FragmentHomeBinding;
+import com.example.slowchien.ui.exchange.ExchangeViewModel;
 import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
@@ -38,6 +43,10 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
+        //HomeViewModel homeViewModel =
+        //        new ViewModelProvider(this).get(HomeViewModel.class);
+        //View root = binding.getRoot();
+
         TabLayout.Tab tab;
 
         View view = inflater.inflate(R.layout.fragment_home,container, false);
@@ -48,6 +57,21 @@ public class HomeFragment extends Fragment {
         TabLayout tabs =  view.findViewById(R.id.tabLayout);
         tabs.setupWithViewPager(viewPager);
 
+        //set new message button text
+        //final TextView textBtnNewMessage = binding.newMessage;
+        //homeViewModel.getNewMessageBtnLib().observe(getViewLifecycleOwner(), textBtnNewMessage::setText);
+
+        // Récupération de la référence au bouton de scan
+        //Button newMessageButton = (Button) view.findViewById(R.id.newMessage);
+/*
+        RelativeLayout.LayoutParams newMessageButtonRelativeLayout =
+                new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        //add rules
+        newMessageButtonRelativeLayout.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        newMessageButtonRelativeLayout.topMargin = 1000;
+
+        newMessageButton.setLayoutParams(newMessageButtonRelativeLayout);
+*/
         // Set icons of the tab bar
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabs));
         tabs.setupWithViewPager(viewPager);
