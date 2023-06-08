669c7800bf25ec730f58f019315380a7539adcc7
loic mouquet <loic.mouquet59@gmail.com>
2023-06-08 16:03:12 +0200

    voila

---
+++ app/src/main/java/com/example/slowchien/ui/contact/Contact.java
@@ -0,0 +1,32 @@
+package com.example.slowchien.ui.contact;
+
+public class Contact {
+
+    private final String name;
+    private final String address;
+    private final String description;
+    private final String macAddress;
+
+    public Contact(String name, String address, String description, String macAddress) {
+        this.name = name;
+        this.address = address;
+        this.description = description;
+        this.macAddress = macAddress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getMacAddress() {
+        return macAddress;
+    }
+}
