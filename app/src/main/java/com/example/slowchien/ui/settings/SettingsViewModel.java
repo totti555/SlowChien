23725692e523a798d19d19846bad1a2a6939dc8f
Myriam-B <myriam.bizart@gmail.com>
2023-06-14 08:44:55 +0200

    popup contact

--- app/src/main/java/com/example/slowchien/ui/settings/SettingsViewModel.java
+++ app/src/main/java/com/example/slowchien/ui/settings/SettingsViewModel.java
@@ -5,18 +5,12 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class SettingsViewModel extends ViewModel {
-    private final MutableLiveData<String> mText;
     private final MutableLiveData<String> mTextBtnClean;
 
     public SettingsViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is settings fragment");
         mTextBtnClean = new MutableLiveData<>();
         mTextBtnClean.setValue("Supprimer les données");
     }
 
-    public LiveData<String> getText() {
-        return mText;
-    }
     public LiveData<String> getCleanBtnLib() { return mTextBtnClean; }
 }
