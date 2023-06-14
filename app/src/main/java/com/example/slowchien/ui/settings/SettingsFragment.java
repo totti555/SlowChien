02833a73e644685a080883e461f5bfad1613274b
Myriam-B <myriam.bizart@gmail.com>
2023-06-14 11:11:37 +0200

    rendre branche visible

--- app/src/main/java/com/example/slowchien/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/slowchien/ui/settings/SettingsFragment.java
@@ -1,10 +1,15 @@
 package com.example.slowchien.ui.settings;
 
+import static java.sql.DriverManager.println;
+
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -15,9 +20,11 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.slowchien.MainActivity;
 import com.example.slowchien.R;
 import com.example.slowchien.databinding.FragmentSettingsBinding;
 import com.example.slowchien.ui.location.JSONUtils;
+import com.google.android.gms.vision.text.Text;
 
 import java.util.Set;
 
@@ -32,17 +39,23 @@ public class SettingsFragment extends Fragment {
         binding = FragmentSettingsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textSettings;
-        settingsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-
         final TextView textBtnBT = binding.ButtonCleanData;
         settingsViewModel.getCleanBtnLib().observe(getViewLifecycleOwner(), textBtnBT::setText);
         Button mScanButton = root.findViewById(R.id.ButtonCleanData);
 
+
+        final TextView textBtnMac = binding.ChangeMacAdress;
+        settingsViewModel.getChangeMACAdress().observe(getViewLifecycleOwner(), textBtnMac::setText);
+        Button mScanButton2 = root.findViewById(R.id.ChangeMacAdress);
+
+         final TextView textViewMACAddress = binding.textSettings;
+        settingsViewModel.getMACAddressText() .observe(getViewLifecycleOwner(), textViewMACAddress::setText);
+        TextView textMacAdress=root.findViewById(R.id.text_settings);
+
         // Ajout d'un écouteur sur le bouton de scan
         // Utilisation dans la méthode onCreateView()
         mScanButton.setOnClickListener(v -> showConfirmationPopup(v));
-
+        mScanButton2.setOnClickListener(v -> changeMacAddress(v,textMacAdress));
         return root;
     }
 
@@ -50,83 +63,149 @@ public class SettingsFragment extends Fragment {
 
     // Fonction pour générer la popup de confirmation
     private void showConfirmationPopup(View anchorView) {
-        PopupWindow popupWindow = new PopupWindow(getActivity());
-        popupWindow.setFocusable(true);
-        popupWindow.setOutsideTouchable(false);
+        Dialog dialog = new Dialog(requireContext(), R.style.RoundDialog);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT
-        );
-        LinearLayout popupLayout = new LinearLayout(getActivity());
-        popupLayout.setLayoutParams(layoutParams);
-        popupLayout.setOrientation(LinearLayout.VERTICAL);
-        popupLayout.setGravity(Gravity.CENTER);
+        int width = getResources().getDisplayMetrics().widthPixels;
+        int dialogWidth = (int) (width * 0.9); // 90% de la largeur de l'écran
+        int dialogHeight = (int) (getResources().getDisplayMetrics().heightPixels * 0.5); // 50% de la hauteur de l'écran
 
+        Window window = dialog.getWindow();
+        if (window != null) {
+            WindowManager.LayoutParams params = window.getAttributes();
+            params.width = dialogWidth;
+            params.height = dialogHeight;
+            window.setAttributes(params);
+            window.setGravity(Gravity.CENTER); // Aligner le dialogue au centre
+        }
 
-        TextView confirmMessage = new TextView(getActivity());
+        LinearLayout dialogLayout = new LinearLayout(requireContext());
+        dialogLayout.setOrientation(LinearLayout.VERTICAL);
+        dialogLayout.setGravity(Gravity.CENTER);
+        dialog.setContentView(dialogLayout);
+
+        TextView confirmMessage = new TextView(requireContext());
         confirmMessage.setText("Voulez-vous vraiment supprimer les données de contact et de message?");
-        popupLayout.addView(confirmMessage);
+        dialogLayout.addView(confirmMessage);
 
-        LinearLayout buttonLayout = new LinearLayout(getActivity());
-        buttonLayout.setLayoutParams(layoutParams);
+        LinearLayout buttonLayout = new LinearLayout(requireContext());
         buttonLayout.setOrientation(LinearLayout.HORIZONTAL);
         buttonLayout.setGravity(Gravity.CENTER);
+        dialogLayout.addView(buttonLayout);
 
         Button yesButton = createButton("Oui", v -> {
-            popupWindow.dismiss();
+            dialog.dismiss();
             JSONUtils.cleanAllJSONFiles(getContext());
-            showResultPopup(anchorView, true);//todo gestion d'erreurs
+            showResultPopup(anchorView, true);
         });
-        buttonLayout.addView(yesButton);
+        LinearLayout.LayoutParams yesButtonParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        yesButtonParams.setMargins(0, 0, 16, 0); // Ajouter une marge à droite
+        yesButton.setLayoutParams(yesButtonParams);
+         buttonLayout.addView(yesButton);
 
-        Button noButton = createButton("Non", v -> popupWindow.dismiss());
+        Button noButton = createButton("Non", v -> dialog.dismiss());
         buttonLayout.addView(noButton);
 
-        popupLayout.addView(buttonLayout);
-
-        popupWindow.setContentView(popupLayout);
-        popupWindow.setWidth(ViewGroup.LayoutParams.WRAP_CONTENT);
-        popupWindow.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
-        popupWindow.showAtLocation(anchorView, Gravity.CENTER, 0, 0);
+        dialog.show();
     }
+
+
+
+
 
     // Fonction pour générer la popup de résultat
     private void showResultPopup(View anchorView, boolean success) {
-        PopupWindow popupWindow = new PopupWindow(getActivity());
-        popupWindow.setFocusable(true);
-        popupWindow.setOutsideTouchable(false);
+        Dialog dialog = new Dialog(requireContext(), R.style.RoundDialog);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT
-        );
-        LinearLayout popupLayout = new LinearLayout(getActivity());
-        popupLayout.setLayoutParams(layoutParams);
-        popupLayout.setOrientation(LinearLayout.VERTICAL);
-        popupLayout.setGravity(Gravity.CENTER);
+        int width = getResources().getDisplayMetrics().widthPixels;
+        int dialogWidth = (int) (width * 0.9); // 90% de la largeur de l'écran
+        int dialogHeight = (int) (getResources().getDisplayMetrics().heightPixels * 0.5); // 50% de la hauteur de l'écran
 
-        TextView resultMessage = new TextView(getActivity());
+        Window window = dialog.getWindow();
+        if (window != null) {
+            WindowManager.LayoutParams params = window.getAttributes();
+            params.width = dialogWidth;
+            params.height = dialogHeight;
+            window.setAttributes(params);
+            window.setGravity(Gravity.CENTER); // Aligner le dialogue au centre
+        }
+
+        LinearLayout dialogLayout = new LinearLayout(requireContext());
+        dialogLayout.setOrientation(LinearLayout.VERTICAL);
+        dialogLayout.setGravity(Gravity.CENTER);
+        dialog.setContentView(dialogLayout);
+
+        TextView resultMessage = new TextView(requireContext());
         if (success) {
             resultMessage.setText("Fichiers nettoyés avec succès");
         } else {
             resultMessage.setText("Échec du nettoyage des fichiers");
         }
-        popupLayout.addView(resultMessage);
+        dialogLayout.addView(resultMessage);
 
-        Button closeButton = createButton("Fermer", v -> popupWindow.dismiss());
-        popupLayout.addView(closeButton);
+        LinearLayout buttonLayout = new LinearLayout(requireContext());
+        buttonLayout.setOrientation(LinearLayout.HORIZONTAL);
+        buttonLayout.setGravity(Gravity.CENTER);
+        dialogLayout.addView(buttonLayout);
 
-        popupWindow.setContentView(popupLayout);
-        popupWindow.setWidth(ViewGroup.LayoutParams.WRAP_CONTENT);
-        popupWindow.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
-        popupWindow.showAtLocation(anchorView, Gravity.CENTER, 0, 0);
+        Button closeButton = createButton("Fermer", v -> dialog.dismiss());
+        buttonLayout.addView(closeButton);
+
+        dialog.show();
     }
+
+    public void changeMacAddress(View anchorView,TextView textMacAdress) {
+        Dialog dialog = new Dialog(requireContext(), R.style.RoundDialog);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        int width = getResources().getDisplayMetrics().widthPixels;
+        int dialogWidth = (int) (width * 0.9); // 90% de la largeur de l'écran
+
+        Window window = dialog.getWindow();
+        if (window != null) {
+            WindowManager.LayoutParams params = window.getAttributes();
+            params.width = dialogWidth;
+            window.setAttributes(params);
+            window.setGravity(Gravity.CENTER); // Aligner le dialogue au centre
+        }
+
+        LinearLayout dialogLayout = new LinearLayout(requireContext());
+        dialogLayout.setOrientation(LinearLayout.VERTICAL);
+        dialogLayout.setGravity(Gravity.CENTER);
+        dialog.setContentView(dialogLayout);
+
+        EditText macAddressEditText = new EditText(requireContext());
+        macAddressEditText.setText("AA:AA:AA:AA:AA:AA"); // Valeur par défaut
+        macAddressEditText.setHint("Adresse MAC");
+        dialogLayout.addView(macAddressEditText);
+
+        LinearLayout buttonLayout = new LinearLayout(requireContext());
+        buttonLayout.setOrientation(LinearLayout.HORIZONTAL);
+        buttonLayout.setGravity(Gravity.CENTER);
+        dialogLayout.addView(buttonLayout);
+
+        Button validateButton = createButton("Valider", v -> {
+            String macAddress = macAddressEditText.getText().toString();
+            // Faites quelque chose avec l'adresse MAC saisie
+            MainActivity.MAC_ADDRESS=macAddress;
+            textMacAdress.setText("Adresse MAC :"+ MainActivity.getMacAddr());
+
+            dialog.dismiss();
+        });
+        buttonLayout.addView(validateButton);
+        dialog.show();
+    }
+
 
     // Fonction pour créer un bouton avec un texte, une couleur de fond et un écouteur
     private Button createButton(String buttonText, View.OnClickListener listener) {
         Button button = new Button(getActivity());
         button.setText(buttonText);
+        button.setBackgroundColor(getResources().getColor(R.color.purple_500)); // Couleur de fond violette
         button.setOnClickListener(listener);
         return button;
     }
--- app/src/main/java/com/example/slowchien/ui/settings/SettingsViewModel.java
+++ app/src/main/java/com/example/slowchien/ui/settings/SettingsViewModel.java
@@ -4,15 +4,23 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.slowchien.MainActivity;
+
 public class SettingsViewModel extends ViewModel {
     private final MutableLiveData<String> mTextBtnClean;
+    private final MutableLiveData<String> mTextBtnChangeMACAdress;
+    private final MutableLiveData<String> mTextMACAddress;
 
     public SettingsViewModel() {
         mTextBtnClean = new MutableLiveData<>();
         mTextBtnClean.setValue("Supprimer les données");
-        //settings
-
+        mTextBtnChangeMACAdress = new MutableLiveData<>();
+        mTextBtnChangeMACAdress.setValue("Modifier la mac adrresse");
+        mTextMACAddress = new MutableLiveData<>();
+        mTextMACAddress.setValue("Adresse MAC :"+ MainActivity.getMacAddr());
     }
 
     public LiveData<String> getCleanBtnLib() { return mTextBtnClean; }
+    public LiveData<String> getChangeMACAdress() { return mTextBtnChangeMACAdress; }
+    public LiveData<String> getMACAddressText() {return mTextMACAddress;}
 }
